baa9b30713ffd4a98fec6728275abb85ab844a91
artem <vonap88@mail.ru>
2023-08-22 16:31:23 +0300

    creatd apps

--- shop/settings.py
+++ shop/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'market',
+    'cart',
+    'orders',
+    'users',
+    'products',
+
 ]
 
 MIDDLEWARE = [
